7384ba59bfdbe06ad88bdf9bcc036ccbe2df81a3
Pooja Yuvaraj <pooja.yuva98@gmail.com>
2025-05-01 15:43:50 -0500

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project - Information Retreival',
+    version = "0.0.0",
+    author = "Pooja Yuvaraj",
+    author_email = "pooja.yuva98@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
